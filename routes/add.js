4d98d88ce4179fced5352faa38ba32b0139be866
David Jasuan <djasuan@ucsd.edu>
2020-02-06 13:42:07 -0800

    LAB5

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,12 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	var addFriend = {
+		"name": request.query.name,
+		"description": request.query.description,
+		"imageURL":"http://lorempixel.com/400/400/people"
+	}
+	data.friends.push(addFriend);
+	response.render('index', data);
+	console.log("New Buddy!");
  }
